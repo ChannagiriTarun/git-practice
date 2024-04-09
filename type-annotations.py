f709c790f3efd7c697a7dd5d4d869afef6870259
channagiritarun <channagiritarun@gmail.com>
2024-04-09 17:09:28 +0530

    pre-commit hook

---
+++ type-annotations.py
@@ -0,0 +1,22 @@
+'''
+This file contains few code snippets indicating type annotations.
+Contains docstrings example also.
+'''
+
+
+def f(a: int) -> None:
+    """
+    Prints the given argument.
+
+    Arguments
+    ---------
+    a: int, no default value
+
+    Returns
+    -------
+    None
+    """
+    print(a)
+
+
+f(1)
